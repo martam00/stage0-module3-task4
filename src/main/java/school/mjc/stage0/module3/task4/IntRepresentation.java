e6440b6c4dbedb07f77491a7e05ec6fe4b37b6fe
martam00 <marta.michniacka1@gmail.com>
2023-07-12 21:52:39 +0200

    fixed

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        // 'c', 'h', 'a', 'r'
+        char a = 97;
+        char c = 99;
+        char h = 104;
+        char r = 114;
+
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
+
     }
 }
